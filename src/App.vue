70fefe9c16ab97a671dbfdf9e351c785111ae40a
Burim Dervisholli <dervisholliburim@gmail.com>
2026-03-11 17:25:48 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const apiKey = import.meta.env.VITE_API_KEY
+</script>
+
+<template>
+  <div>
+    <QBtn color="primary" label="Hello World" />
+    {{ apiKey }}
+    <h1>You did it! <q-badge color="secondary"> #4D96F2 </q-badge></h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+import App from './App.vue'
+import router from './router'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(Quasar)
+
+app.mount('#app')
